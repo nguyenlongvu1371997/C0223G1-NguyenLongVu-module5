7a77a8501628f3bffec125f9ad3133573ff09d3b
Dell <nguyenlongvu1371997@gmail.com>
2023-07-17 13:39:06 +0700

    em nop bai tap ngay 17/7

---
+++ ss1_ES6/es6.js
@@ -0,0 +1,65 @@
+let courses = [
+  {
+    id: 1,
+    title: "ReactJS Tutorial",
+    rating: 4.2,
+  },
+  {
+    id: 2,
+    title: "Angular Tutorial",
+    rating: 2.5,
+  },
+  {
+    id: 3,
+    title: "VueJS Tutorial",
+    rating: 3.8,
+  },
+  {
+    id: 4,
+    title: "Java Tutorial",
+    rating: 4,
+  },
+  {
+    id: 5,
+    title: "JavaScript Tutorial",
+    rating: 3.5,
+  },
+];
+
+const goodCourses = courses.filter(courses => courses.rating >= 4);
+console.log(goodCourses);
+
+const badCourses = [];
+
+courses.filter(courses => courses.rating < 4).forEach(e => {
+  badCourses.push(e.id + '-' + e.title + '-' + e.rating);
+});
+
+console.log(badCourses);
+
+let addedCourses = [
+  {
+    id: 6,
+    title: "PHP Tutorial",
+    rating: 3,
+  },
+  {
+    id: 7,
+    title: "C# Tutorial",
+    rating: 2,
+  },
+  {
+    id: 8,
+    title: "Docker Tutorial",
+    rating: 3.8,
+  }
+];
+
+const mergeArray = (arr1, arr2) => {
+  return [...arr1, ...arr2];
+};
+
+console.log(mergeArray(courses, addedCourses))
+
+
+
